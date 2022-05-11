e204cc23a58da5e0fe11ba759c4e1031370dfe85
cdot <github@c-dot.co.uk>
2022-05-11 13:11:50 +0100

    Normalise quotes

--- editions/Tiny.js
+++ editions/Tiny.js
@@ -1,45 +1,45 @@
 // Test edition
 // 11x11 board with all tile values
 // English letters, used with Custom_English.dict
-define('editions/Tiny', () => {
+define("editions/Tiny", () => {
 
 	const BOARD = [
-		'M_____',
-		'_d____',
-		'__t___',
-		'___T__',
-		'____q_',
-		'_____Q',
+		"M_____",
+		"_d____",
+		"__t___",
+		"___T__",
+		"____q_",
+		"_____Q",
 	];
 
 	const BAG = [
 		{ score: 0, count: 1 },
-		{ letter: 'A', score: 1, count: 5 },
-		{ letter: 'B', score: 3, count: 1 },
-		{ letter: 'C', score: 3, count: 1 },
-		{ letter: 'D', score: 2, count: 2 },
-		{ letter: 'E', score: 1, count: 5 },
-		{ letter: 'F', score: 3, count: 1 },
-		{ letter: 'G', score: 2, count: 1 },
-		{ letter: 'H', score: 2, count: 1 },
-		{ letter: 'I', score: 1, count: 4 },
-		{ letter: 'J', score: 4, count: 1 },
-		{ letter: 'K', score: 4, count: 1 },
-		{ letter: 'L', score: 1, count: 3 },
-		{ letter: 'M', score: 3, count: 1 },
-		{ letter: 'N', score: 1, count: 3 },
-		{ letter: 'O', score: 1, count: 4 },
-		{ letter: 'P', score: 3, count: 1 },
-		{ letter: 'Q', score: 4, count: 1 },
-		{ letter: 'R', score: 1, count: 5 },
-		{ letter: 'S', score: 1, count: 5 },
-		{ letter: 'T', score: 1, count: 4 },
-		{ letter: 'U', score: 1, count: 3 },
-		{ letter: 'V', score: 4, count: 1 },
-		{ letter: 'W', score: 3, count: 1 },
-		{ letter: 'X', score: 2, count: 1 },
-		{ letter: 'Y', score: 2, count: 1 },
-		{ letter: 'Z', score: 4, count: 1 }
+		{ letter: "A", score: 1, count: 5 },
+		{ letter: "B", score: 3, count: 1 },
+		{ letter: "C", score: 3, count: 1 },
+		{ letter: "D", score: 2, count: 2 },
+		{ letter: "E", score: 1, count: 5 },
+		{ letter: "F", score: 3, count: 1 },
+		{ letter: "G", score: 2, count: 1 },
+		{ letter: "H", score: 2, count: 1 },
+		{ letter: "I", score: 1, count: 4 },
+		{ letter: "J", score: 4, count: 1 },
+		{ letter: "K", score: 4, count: 1 },
+		{ letter: "L", score: 1, count: 3 },
+		{ letter: "M", score: 3, count: 1 },
+		{ letter: "N", score: 1, count: 3 },
+		{ letter: "O", score: 1, count: 4 },
+		{ letter: "P", score: 3, count: 1 },
+		{ letter: "Q", score: 4, count: 1 },
+		{ letter: "R", score: 1, count: 5 },
+		{ letter: "S", score: 1, count: 5 },
+		{ letter: "T", score: 1, count: 4 },
+		{ letter: "U", score: 1, count: 3 },
+		{ letter: "V", score: 4, count: 1 },
+		{ letter: "W", score: 3, count: 1 },
+		{ letter: "X", score: 2, count: 1 },
+		{ letter: "Y", score: 2, count: 1 },
+		{ letter: "Z", score: 4, count: 1 }
 	];
 
 	return {
--- editions/_Scrabble.js
+++ editions/_Scrabble.js
@@ -2,21 +2,21 @@
  * Layout of a scrabble board as used by all classic Scrabble(R)
  * editions
  */
-define('editions/_Scrabble', () => {
+define("editions/_Scrabble", () => {
 	// Return a function, just in case different languages are used on
 	// the same server. Each needs its own unique Edition.
 	return () => {
 		return {
 			// Layout of lower-right quadrant, including middle row/col
 			layout: [
-				'M___d__T',
-				'_d___d__',
-				'__t___t_',
-				'___D____',
-				'd___D__d',
-				'_d___D__',
-				'__t___D_',
-				'T___d__T',
+				"M___d__T",
+				"_d___d__",
+				"__t___t_",
+				"___D____",
+				"d___D__d",
+				"_d___D__",
+				"__t___D_",
+				"T___d__T",
 			],
 			swapCount: 7,      // tiles on rack
 			rackCount: 7,      // tiles swappable in a move
