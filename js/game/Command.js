09fb8a7d106a8688fa5ce26b0804d6086e1b2868
cdot <github@c-dot.co.uk>
2022-05-11 13:08:00 +0100

    Add enumerated types to cut down on confusion

---
+++ js/game/Command.js
@@ -0,0 +1,18 @@
+/*Copyright (C) 2019-2022 The Xanado Project https://github.com/cdot/Xanado
+License MIT. See README.md at the root of this distribution for full copyright
+and license information*/
+/* eslint-env amd */
+
+define("game/Command", () => {
+
+	return {
+		UNPAUSE: "unpause",
+		PAUSE: "pause",
+		CHALLENGE: "challenge",
+		PLAY: "play",
+		TAKE_BACK: "takeBack",
+		PASS: "pass",
+		GAME_OVER: "confirmGameOver",
+		SWAP: "swap"
+	};
+});
---
+++ js/game/Notify.js
@@ -0,0 +1,23 @@
+/*Copyright (C) 2019-2022 The Xanado Project https://github.com/cdot/Xanado
+License MIT. See README.md at the root of this distribution for full copyright
+and license information*/
+/* eslint-env amd */
+
+define("game/Notify", () => {
+
+	return {
+		UNPAUSE: "unpause",
+		PAUSE: "pause",
+		JOIN: "join",
+		REJECT: "reject",
+		MESSAGE: "message",
+		NEXT_GAME: "nextGame",
+		ANOTHER_GAME: "anotherGame",
+		TICK: "tick",
+		TURN: "turn",
+		CONNECTIONS: "connections",
+
+		UPDATE: "update",
+		MONITOR: "monitor"
+	};
+});
