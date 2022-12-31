133f5842f8a2b156060af2f3ba28b7e09f8c9fbf
cdot <github@c-dot.co.uk>
2022-12-31 18:10:59 +0000

    Get rid of dist dependency on node_modules.

--- build/webpack_config.js
+++ build/webpack_config.js
@@ -6,13 +6,55 @@ import TerserPlugin from "terser-webpack-plugin";
 import webpack from "webpack";
 import { promises as fs } from "fs";
 
+function copyFile(from, to) {
+  const a_from = path.normalize(path.join(__dirname, from));
+  const a_to = path.normalize(path.join(__dirname, to));
+  fs.cp(a_from, a_to, {
+    recursive: true,
+    force: true,
+//    filter: f => { console.debug("copy", f); return true; },
+    dereference: true
+  })
+  .catch(e => {
+    // cp works, but throws all sorts of wierd errors for no
+    // apparent reason before completing.
+    //console.error("wierd", from, e);
+  });
+}
+
+function relink(from, to, content) {
+  const re = new RegExp(`(<link[^>]*href=")${from}`, "g");
+  return content.replace(
+    re,
+    (m, preamble) => `${preamble}${to}`);
+}
+
 function makeConfig(html, js) {
 
   fs.readFile(`${__dirname}/../html/${html}`)
   .then(content => {
-    content = content.toString().replace(
-      /(<script type="module" src=").*?"/,
-      `$1../dist/${js}"`);
+    content = content.toString();
+
+    // Reroute the <script call to the main module.
+    // There can be only one!
+    content = content.replace(
+      /(<script type="module" src=").*?([^/]+\/[^/]+.js")/,
+      "$1../dist/$2");
+
+    // Pull nrcessary CSS files out of node_modules; they may not be
+    // installed on the target platform
+    copyFile("../node_modules/normalize.css/normalize.css",
+             "../dist/css/normalize.css");
+    content = relink("../node_modules/normalize.css/normalize.css",
+             "../dist/css/normalize.css",
+            content);
+
+    copyFile("../node_modules/jquery-ui/dist/themes",
+             "../dist/css/themes");
+    content = relink("../node_modules/jquery-ui/dist/themes",
+            "../dist/css/themes",
+            content);
+
     return fs.writeFile(`${__dirname}/../dist/${html}`, content);
   });
 
